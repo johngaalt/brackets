b2d531c1e39c748bee3dd9a15922b9dce3032563
johngaalt <rajon777666@gmail.com>
2023-02-06 15:32:55 +0300

    init: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(string, config) {
+  let stack = [];
+
+  for (let i = 0; i < string.length; i++) {
+    let currentChar = string.charAt(i);
+
+    for (let j = 0; j < config.length; j++) {
+      let openBracket = config[j][0];
+      let closeBracket = config[j][1];
+
+      if (currentChar === openBracket) {
+        stack.push(closeBracket);
+        break;
+      } else if (currentChar === closeBracket) {
+        if (stack.length === 0 || stack[stack.length - 1] !== closeBracket) {
+          return false;
+        }
+        stack.pop();
+        break;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
